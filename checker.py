6165bf2c79ca87de6789face82f6f4e08939a35d
Syed Shayan Arshad <syedshayanarshad.1@gmail.com>
2025-05-15 22:10:15 +0500

    Update checker.py

--- checker.py
+++ checker.py
@@ -8,7 +8,7 @@ import requests  # Still needed for Telegram API
 
 def send_telegram_message(message):
     bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-    chat_id = 'os.getenv('TELEGRAM_CHAT_ID')
+    chat_id = os.getenv('TELEGRAM_CHAT_ID')
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
         'chat_id': chat_id,
